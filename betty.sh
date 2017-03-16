6f6b1a1356c98d65276b751f7fd770dc84613ad9
Ilyas Deckers <ilyas@phasehosting.io>
2017-03-16 22:42:57 +0100

    Create betty.sh

---
+++ betty.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+#PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Hosted Zone ID e.g. BJBK35SKMM9OE
+ZONEID="Z1395DSCRDTCOQ"
+
+# The CNAME you want to update e.g. hello.example.com
+RECORDSET="betty.phasehosting.io"
+
+TTL=10
+COMMENT="Auto updating @ `date`"
+TYPE="A"
+
+IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOGFILE="$DIR/update-route53.log"
+IPFILE="$DIR/update-route53.ip"
+
+if ! valid_ip $IP; then
+    echo "Invalid IP address: $IP" >> "$LOGFILE"
+    exit 1
+fi
+
+# Check if the IP has changed
+if [ ! -f "$IPFILE" ]
+    then
+    touch "$IPFILE"
+fi
+
+if grep -Fxq "$IP" "$IPFILE"; then
+    # code if found
+    echo "IP is still $IP. Exiting" >> "$LOGFILE"
+    exit 0
+else
+    echo "IP has changed to $IP" >> "$LOGFILE"
+    # Fill a temp file with valid JSON
+    TMPFILE=$(mktemp /tmp/temporary-file.XXXXXXXX)
+    cat > ${TMPFILE} << EOF
+    {
+      "Comment":"$COMMENT",
+      "Changes":[
+        {
+          "Action":"UPSERT",
+          "ResourceRecordSet":{
+            "ResourceRecords":[
+              {
+                "Value":"$IP"
+              }
+            ],
+            "Name":"$RECORDSET",
+            "Type":"$TYPE",
+            "TTL":$TTL
+          }
+        }
+      ]
+    }
+EOF
+
+    # Update the Hosted Zone record
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONEID \
+        --change-batch file://"$TMPFILE" >> "$LOGFILE"
+    echo "" >> "$LOGFILE"
+
+    # Clean up
+    rm $TMPFILE
+fi
+
+# All Done - cache the IP address for next time
+echo "$IP" > "$IPFILE"
